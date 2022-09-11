bf87f66e8746a8f2ddaa03d3b832c82a498c9f84
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2022-09-11 22:01:51 +0700

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p class="brown darken-2 white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+      voluptates commodi explicabo beatae, fuga ducimus molestias voluptas odio
+      illo aliquid sit libero cumque officiis, molestiae dolore magni sequi
+      autem? Fugiat!
+    </p>
+
+    <p class="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+      omnis distinctio dolorem doloribus repellendus eveniet id temporibus, nam
+      voluptatum sed earum quae hic saepe, voluptate odit molestiae vero autem
+      voluptatibus.
+    </p>
+
+    <p class="text-md-h1">This is H1</p>
+
+    <p class="title">This is title</p>
+    <p class="subtitle-1">This is sub title 1</p>
+    <p class="subtitle-2">This is sub title 2</p>
+
+    <p class="caption">This is caption</p>
+
+    <div class="text-no-wrap primary" style="width: 10rem">
+      This text should overflow the parent.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
