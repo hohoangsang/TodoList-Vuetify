e326675f9df0922660eb59ef9136b8b93672e853
Ho Hoang Sang <hoangsangho93@gmail.com>
2022-09-12 11:26:29 +0700

    complete basic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+
+    <!-- Text-color -->
     <p class="brown darken-2 white--text">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
       voluptates commodi explicabo beatae, fuga ducimus molestias voluptas odio
@@ -15,7 +17,7 @@
       voluptatibus.
     </p>
 
-    <p class="text-md-h1">This is H1</p>
+    <p class="text-md-h1 text-sm-h3 hidden-xs-only">This is H1</p>
 
     <p class="title">This is title</p>
     <p class="subtitle-1">This is sub title 1</p>
@@ -23,9 +25,40 @@
 
     <p class="caption">This is caption</p>
 
-    <div class="text-no-wrap primary" style="width: 10rem">
-      This text should overflow the parent.
+    <div class="font-weight-bold text-center">
+      Test text.
     </div>
+
+    <!-- button & icon -->
+    <v-btn :loading="false" class="red accent-4 white--text mr-2">Click me</v-btn>
+
+    <v-btn depressed dark color="pink" class="mr-2">Click me</v-btn>
+
+    <v-btn plain color="pink" class="mr-2">Click me</v-btn>
+
+    <v-btn class="red accent-4 white--text mr-2 text-capitalize" large>
+      <v-icon left large>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn class="red accent-4 white--text mr-2" small>
+      <span>Call me</span>
+      <v-icon small right>mdi-phone</v-icon>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+
+    <v-btn class="my-2 mx-2" depressed dark fab small>
+      <v-icon small>mdi-format-list-bulleted-square</v-icon>
+    </v-btn>
+
+    <v-btn class="my-2 mx-2" depressed dark fab>
+      <v-icon>mdi-format-list-bulleted-square</v-icon>
+    </v-btn>
+
+    <v-btn class="my-2 mx-2" depressed dark fab large color="purple">
+      <v-icon large>mdi-format-list-bulleted-square</v-icon>
+    </v-btn>
   </div>
 </template>
 
